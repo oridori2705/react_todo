8ebfd3a2be0a542f1ae65b369ce07888cec13cc9
신준혁 <oridori2705@naver.com>
2023-02-10 22:37:05 +0900

    할 일 목록 제거하기 최종 수정
    할 일 목록 제거하기 최종 수정

--- src/App.js
+++ src/App.js
@@ -19,39 +19,43 @@ export default class App extends Component{
         textDecoration : "none"
       }
   }
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    },
-  ]
+  state={
+      todoData :[
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false
+      },
+    ],
+    value : ""
+  }
   handleClick=(id)=>
   {
     //this.todoData => this.state.todoDota로 바꿔줘야함 호출한 곳도 바꿔줘야함
-    let newTodoData = this.state.todoData.filter(data=>data.id !=id)
+    let newTodoData = this.state.todoData.filter(data=>data.id !==id)
     this.setState({todoData: newTodoData}); //todoData를 newTodoData로 업데이트
   } 
+
   render(){
     return(
-      <div calssName="container">
-        <div className="todoBlock">
-          <div className="title">
+      <div>
+        <div>
+          <div>
             <h1>할 일 목록</h1>
           </div>
-          {this.todoData.map((data)=>( //data라는 변수로 선언
+          {this.state.todoData.map((data)=>( //data라는 변수로 선언
           //<p></p> 태그는 paragraph, 즉 문단의 약자로, 하나의 문단을 만들 때 쓰입니다. 없어도 됨
           //나열을 해줄 때는 key속성으로 배열의 유니크한 값으로 지정한다.
             <div style={this.getStyle()} key={data.id}>
               <p> 
                 <input type="checkbox" defaultChecked={false}/>
                 {data.title}
-                <button style={this.btnStyle}>x</button>
+                <button style={this.btnStyle} onClick={()=>this.handleClick(data.id)}>x</button>
               </p>
               </div>
           ))}
