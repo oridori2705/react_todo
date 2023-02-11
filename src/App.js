54015c1c11567a03b181f3e15fbbeead57fa4b7b
신준혁 <oridori2705@naver.com>
2023-02-11 20:51:56 +0900

    할 일 목록 모두 제거하기 버튼 추가
    할 일 목록 모두 제거하기 버튼 추가

--- src/App.js
+++ src/App.js
@@ -18,17 +18,17 @@ export default function App (){
       completed : false
     }
     setTodoData(prev=>[...prev,newTodo]); //인수에 함수를 이용하면 prev가 이전의 todoData 값을 가져올 수 있다.
+  }
 
-}
-  
-
-  
-  
+  const handleRemoveClick=()=>{
+    setTodoData([]);
+  }
   return(
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-5 m-4 bg-white rounded shadow-sm lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
         <Lists todoData={todoData} setTodoData={setTodoData}/>
         <Form handleSubmit={handleSubmit} value={value} setvalue={setvalue}/>
